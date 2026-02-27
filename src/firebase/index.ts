4a438ac002a53b73e75c2ea8e295c6f2dbbfba5e
Chimbueze David <chimdaveo@gmail.com>
2026-02-27 19:47:19 +0000

    Try fixing this error: `Console Error: [2026-02-27T19:45:54.986Z] @fireb

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,8 +1,13 @@
+
 import { getApps, initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
 import { firebaseConfig } from './config';
 import { FirebaseErrorListener } from '@/components/FirebaseErrorListener';
+
+let firebaseApp: FirebaseApp | null = null;
+let auth: Auth | null = null;
+let firestore: Firestore | null = null;
 
 function initializeFirebase() {
   if (
@@ -15,13 +20,14 @@ function initializeFirebase() {
     );
   }
 
-  const apps = getApps();
-  const app = apps.length ? apps[0] : initializeApp(firebaseConfig);
+  if (!firebaseApp) {
+    const apps = getApps();
+    firebaseApp = apps.length ? apps[0] : initializeApp(firebaseConfig);
+    auth = getAuth(firebaseApp);
+    firestore = getFirestore(firebaseApp);
+  }
 
-  const auth = getAuth(app);
-  const firestore = getFirestore(app);
-
-  return { firebaseApp: app, auth, firestore };
+  return { firebaseApp, auth, firestore };
 }
 
 import {
